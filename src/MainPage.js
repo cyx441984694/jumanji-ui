2139e07753986f43a95964b3f044e96a077ce8e4
cyx441984694 <441984694@qq.com>
2023-11-25 15:50:30 +0800

    add layout

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { Layout } from 'antd';
+const { Content } = Layout;
 
 
 function MainPage() {
   return (
-    <div className="MainPage">
-      <div className="MainPage-header">
-        <h2> Hello world !</h2>
-        <br></br>
-        <h3>This is a prototype of pipeline orchestraction</h3>
-      </div>
-    </div>
+    <Layout>
+        <Content style={{ padding: '0 50px',minHeight: "85vh",background: "#fff", }}>
+        <div>
+            <div>
+                <h2> Hello world !</h2>
+                <br></br>
+                <h3>This is a prototype of pipeline orchestraction</h3>
+            </div>
+        </div>        
+        </Content>
+    </Layout>
   );
 }
 
--- src/Setting.js
+++ src/Setting.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { Layout } from 'antd';
 
+
+const { Content } = Layout;
 
 function Setting() {
-
-
   return (
-    <div className="Setting">
-      <div className="Setting-header">
-        <h2> Hello world !</h2>
-        <br></br>
-        <h3>Setting</h3>
-      </div>
-    </div>
+    <Layout>
+        <Content style={{ padding: '0 50px',minHeight: "85vh",background: "#fff", }}>
+        <div>
+            <div>
+                <h2> Hello world !</h2>
+                <br></br>
+                <h3>Setting</h3>
+            </div>
+        </div>        
+        </Content>
+    </Layout>
   );
 }
 
